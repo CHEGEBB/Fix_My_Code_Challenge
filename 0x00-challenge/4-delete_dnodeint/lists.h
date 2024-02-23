177c6c8089289783b06c29a87ac785da216af3d0
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-23 20:33:25 +0300

    Update lists.h

--- 0x00-challenge/4-delete_dnodeint/lists.h
+++ 0x00-challenge/4-delete_dnodeint/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stddef.h>
 
@@ -14,9 +14,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -24,4 +24,4 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void free_dlistint(dlistint_t *head);
 
-#endif /* LISTS_H */
+#endif
