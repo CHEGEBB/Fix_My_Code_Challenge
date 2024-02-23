c6dce585c746872de235bcf02c7b0d427dbdb379
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-23 20:30:36 +0300

    Update 2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,16 +1,17 @@
 ###
-# Sort integer arguments (ascending)
+#
+#  Sort integer arguments (ascending) 
+#
 ###
 
 result = []
-
 ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
-
+    
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -19,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i, i_arg) # corrected the index here
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
