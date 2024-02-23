5d89238f21793b927343cc4e12a9bd10affd1378
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-02-23 20:34:37 +0300

    Update print_dlistint.c

--- 0x00-challenge/4-delete_dnodeint/print_dlistint.c
+++ 0x00-challenge/4-delete_dnodeint/print_dlistint.c
@@ -2,22 +2,22 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints a doubly linked list of integers
+ * print_dlistint - Prints a doubly linkedlist of integers
  *
- * @head: A pointer to the first element of a list
+ * @h: A pointer to the first element of a list
  *
- * Return: The number of elements printed
+ * Return: The number of element printed
  */
-size_t print_dlistint(const dlistint_t *head)
+size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n;
+	size_t n;
 
-    n = 0;
-    while (head)
-    {
-        printf("%d\n", head->n);
-        head = head->next;
-        n++;
-    }
-    return (n);
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
